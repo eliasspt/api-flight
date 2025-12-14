dc5377696ccaebc030ba51c0adc4d039fc49cd6f
eliasspt <eliasspt@gmail.com>
2025-12-14 04:25:03 -0400

    chore: Remove PATCH and OPTIONS from allowed CORS methods in CORS headers.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
   header('Content-Type: application/json; charset=utf-8');
   header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
   header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
   Flight::route('POST /auth', [$usuarios, 'auth']);
